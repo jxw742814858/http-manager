8fc9deb54d7e143cfb3f36e4951de28b326149d8
jiangxiaowei <742814858@qq.com>
2018-06-12 11:07:36 +0800

    代码注释更新_18061203

--- http-server/src/main/java/cn/iamtudou/kit/EscapeKit.java
+++ http-server/src/main/java/cn/iamtudou/kit/EscapeKit.java
@@ -1,11 +1,19 @@
 package cn.iamtudou.kit;
 
+/**
+ * 字符转义工具
+ */
 public class EscapeKit {
 
     public static final String[] ESCAPE_SPECIAL_CHARS = {
             "\\", "$", "(", ")", "*", "+", ".", "[", "]", "?", "^", "{", "}", "|"
     };
 
+    /**
+     * 为指定字符添加转义符号
+     * @param context
+     * @return
+     */
     public static String convert(String context) {
         for (int i = 0, length = ESCAPE_SPECIAL_CHARS.length; i < length; i++) {
             context = context.replace(ESCAPE_SPECIAL_CHARS[i], "\\" + ESCAPE_SPECIAL_CHARS[i]);
